3c645bc6a79d550a470a433daeee816ad82d08d9
Duc Doan <doan_d1@denison.edu>
2022-01-23 22:52:58 -0600

    fix update page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import json
+import requests
 
 app = Flask(__name__)
 
@@ -8,33 +10,34 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
     dateCreated = db.Column(db.DateTime, default=datetime.utcnow)
 
-    # def __init__(self,id,content,completed,dateCreated):
-    #     self.id = id
-    #     self.content = content
+    def __init__(self, content):
+        #     self.id = id
+        self.content = content
     #     self.completed = completed
     #     self.dateCreated = dateCreated
-    
+
     def __repr__(self):
-        return "<Task %r>" % self.id    
+        return "<Task %r>" % self.id
+
 
 @app.route("/", methods=["POST", "GET"])
-
-def index(): 
+def index():
     if request.method == "POST":
         task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        new_task = Todo(task_content)
 
-        try: 
+        try:
             db.session.add(new_task)
             db.session.commit()
             return redirect("/")
-        except: 
+        except:
             return "Cannot post the new message!!!"
 
     else:
@@ -42,37 +45,47 @@ def index():
         return render_template("index.html", tasks=tasks)
 
 
-#delete Route
-@app.route("/delete/<id>")
-
-#delete function
+# delete Route
+@app.route("/delete/<int:id>")
+# delete function
 def delete(id):
     delete_task = Todo.query.get_or_404(id)
-    
+
     try:
         db.session.delete(delete_task)
         db.session.commit()
         return redirect("/")
-    
-    except: 
-        return "There was an Error when deleting the Task"
-    
-#Update Task Function
-@app.route("/update/<id>", methods=["POST","GET"])
 
+    except:
+        return "There was an Error when deleting the Task"
+
+# Update Task Function
+
+
+@app.route("/update/<int:id>", methods=["POST", "GET"])  # dynamics URL
 def update(id):
-    update_task = Todo.query.get_or_404(id)
+    updateTask = Todo.query.get_or_404(id)
+    print(updateTask.content)
 
     if request.method == "POST":
-        update = request.form["update"]   
-        update_task.content = update   #update the content prop of the update_task obj
+        update = request.form["update"]
+        updateTask.content = update  # update the content prop of the update_task obj
         try:
-            db.session.commit()    #remember to commit whenever you change anything in the db.          
+            # remember to commit whenever you change anything in the db.
+            db.session.commit()
             return redirect("/")
-        except: 
+        except:
             return "Cannot update the task!!!"
     else:
-        return render_template("update.html", task=update_task)
+        return render_template("update.html", task=updateTask)
+
+
+# Fetch data from an API
+@app.route("/user", methods=["GET"])
+def fetchUser():
+    req = requests.get("https://jsonplaceholder.typicode.com/users")
+    data = json.loads(req.content)
+    return render_template("user.html", users=data)
 
 
 if __name__ == "__main__":
